672ec13c3250da24cbf8572f005fa150329e9858
Alex Chamara <cb011374@students.apiit.lk>
2025-02-03 13:47:27 +0530

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,19 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                "resources/css/welcome.css",
+                "resources/css/app.css",
+                "resources/css/about.css",
+                "resources/css/service.css",
+                "resources/css/sign.css",
+                "resources/css/dashboard.css",
+                "resources/css/advert.css",
+                "resources/css/shop.css",
+            ],
             refresh: true,
         }),
     ],
